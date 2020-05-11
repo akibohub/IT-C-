4efe7bc8051258b10a6c9d9a347cab906367e4d4
Akibo <k_akibo@yahoo.co.jp>
2020-05-11 10:23:39 +0900

    プロジェクト ファイルを追加します。

---
+++ test01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Hello");
+            //Console.ReadLine();
+            //var message = "welcome to the csharp's world";
+            //Console.WriteLine(message);
+            Random random = new Random();
+            var password = string.Empty;
+            for(var i = 0; i < 10; i++)
+            {
+                password += random.Next(10);
+            }
+            Console.WriteLine(password);
+
+
+
+        }
+    }
+}
