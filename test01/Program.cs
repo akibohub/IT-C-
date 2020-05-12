d30366637de2c8d1b4cf805fc809f035ec1fe2be
Akibo <k_akibo@yahoo.co.jp>
2020-05-12 09:09:47 +0900

    Chapter1-3 英字をランダムに 生成せよ！

--- test01/Program.cs
+++ test01/Program.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Chapter1-3 英字をランダムに	生成せよ！
+/// </summary>
 namespace test01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int count = 14;
-            var password = MakePassword(count);//MakePasswordメソッドを呼び出す
+            var count = 10;
+            //var password = MakeLowerAlphabet(count);
+            var password = MakeUpperAlphabet(count);
             Console.WriteLine(password);
+        }       
+        private static string MakeUpperAlphabet(int count)
+        {
+            //英大文字をランダムに生成するメソッド　文字数は引数countの値に準ずる
+            Random random = new Random();
+            var password = string.Empty;
+            for(var i = 0; i < count; i++)
+            {
+                var n = random.Next(65, 91);
+                var c = (char)n;
+                password += c.ToString();
+            }
+            return password;
+        }
+        private static string MakeLowerAlphabet(int count)
+        {
+            //英小文字をランダムに生成するメソッド　文字数は引数countの値に準ずる
+            Random random = new Random();
+            string password = string.Empty;
+            for (var i = 0; i < count; i++)
+            {
+                var n = random.Next(97, 123);
+                var c = (char)n;
+                password += c.ToString();
+            }
+            return password;
         }
         private static string MakePassword(int count)
         {
+            //数字をランダムに生成するメソッド　文字数は引数countの値に準ずる
             Random random = new Random();
             var password = string.Empty;
             for (var i = 0; i < count; i++)//引数countの値を繰り返しの回数に使用している
